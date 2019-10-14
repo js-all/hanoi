45e63af2536fbd59890da16652f0bb0e5f50a84a
js-all <titouangoulois29@gmail.com>
2019-10-14 22:33:01 +0300

    globals fonction moved in utils.ts

---
+++ ts/utils.ts
@@ -0,0 +1,25 @@
+
+function roundRect(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: Radius, fill = true, stroke = true) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius.tl, y);
+    ctx.lineTo(x + width - radius.tr, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    ctx.lineTo(x + width, y + height - radius.br);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    ctx.lineTo(x + radius.bl, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    ctx.lineTo(x, y + radius.tl);
+    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    ctx.closePath();
+    if (fill) {
+        ctx.fill();
+    }
+    if (stroke) {
+        ctx.stroke();
+    }
+
+}
+
+function createRad(radFac :number) {
+    return { tl: radFac, tr: radFac, bl: radFac, br: radFac }
+}
